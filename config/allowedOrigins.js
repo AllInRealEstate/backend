9e9cc9b1ce5df5f6dc318baefb2ac100acb6f783
Majd Salameh <majdsalameh9@gmail.com>
2026-03-11 01:15:22 +0200

    add admin domain

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -8,7 +8,8 @@ const allowedOrigins = [
   "https://www.allinrealestate.net",
   "https://allinrealestate.net",
   "https://admin.allinrealestate.net",
-  "https://beautiful-smile-production-e832.up.railway.app"
+  "https://beautiful-smile-production-e832.up.railway.app",
+  "https://admin-production-3a6f.up.railway.app"
 ];
 
 module.exports = allowedOrigins;
